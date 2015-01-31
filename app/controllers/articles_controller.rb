9b6439229eeab014245d357546d7b99c120b010e
Dhruti Shah <dhrutishah10@gmail.com>
2015-01-31 18:31:10 +0300

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,10 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
+    @comment = Comment.new
+    @comment.article_id = @article.id
+
   end
 
   def new
@@ -27,7 +31,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to article_path(@article)
+    redirect_to articles_path
   end
 
   def edit
