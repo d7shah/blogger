f0ee097aa806fe7f71b6279bad5dd3118e6e8129
Dhruti Shah <dhrutishah10@gmail.com>
2015-01-31 16:35:55 +0300

    added _form file and altered view. new and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,34 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-end
+  def new
+    @article = Article.new
+  end
+
+  def create
+       @article = Article.new(article_params)
+       @article.save
+       redirect_to article_path(@article)
+
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
+  private
+
+  def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+ end
+
+
