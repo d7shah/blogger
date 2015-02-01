f0777f1d648514ecfe46f1a6e934bc5d0dc0e20c
Dhruti Shah <dhrutishah10@gmail.com>
2015-02-01 13:54:35 +0300

    finished tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,7 +52,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :tag_list)
     end
 
  end
